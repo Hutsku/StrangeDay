0f62bebf16c4d8a585a453fa7e56c12971526158
arouxel <46692157+Hutsku@users.noreply.github.com>
2024-03-07 21:59:25 +0100

    Option fdp gratos

--- cart.js
+++ cart.js
@@ -7,8 +7,11 @@ function roundPrice(x) {
 // Renvoit le frais de port selon le pays et code postal (Colissimo)
 function getShippingCost(country, postal_code, weight) {
     // Si le client habite à Plaisir, élancourt ou aux clayes, frais gratuit
-    let free = ['78370', '78990', '78340']
-    if (free.indexOf(postal_code) >= 0) return 0;
+    // let free = ['78370', '78990', '78340']
+    // if (free.indexOf(postal_code) >= 0) return 0;
+
+    // Si la panier ne pèse rien (par exemple pour des affiches), fdp gratos
+    if (weight == 0) return 0;
     
     // On définie à l'avance les prix
     let metroCost  = [4.99, 6.99, 8.1, 8.80];     // (250mg, 500, 750, 1Kg)
